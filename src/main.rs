7f726a6c60f214d0aeb8dbab47c0550c1f1bf2c2
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-12-15 04:58:51 +0200

    feat: precise eta

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,7 @@ fn main() -> Result<()> {
     }
 
     let pb = ProgressBar::new(0);
-        pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta_precise})")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
